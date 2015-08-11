5794f706a8d05cffcf0721b38e4cbd0fe30df129
Anh Pham <anh-pham@kayac.com>
2015-08-11 18:56:08 +0900

    Fixed bug SceneLegacyAnimation when Time.timeScale = 0

--- Assets/SSSystem/Scripts/Animation/SceneLegacyAnimation.cs
+++ Assets/SSSystem/Scripts/Animation/SceneLegacyAnimation.cs
@@ -11,6 +11,16 @@ namespace SS
         [SerializeField]
         protected string m_HideAnimName = "Hide";
 
+        float m_TimeAtLastFrame = 0F;
+        float m_TimeAtCurrentFrame = 0F;
+        float m_DeltaTime = 0F;
+        float m_AccumTime = 0F;
+
+        AnimationState m_CurrState;
+        bool m_IsPlaying = false;
+        bool m_IsEndAnim = false;
+        string m_CurrClipName;
+
         Animation m_Animation;
         Animation Animation
         {
@@ -43,24 +53,109 @@ namespace SS
 
         public override void Show(bool hasAnimation)
         {
-            Animation.Play(m_ShowAnimName);
-
             if (!hasAnimation)
             {
+                Animation.Play(m_ShowAnimName);
                 Animation[m_ShowAnimName].time = Animation[m_ShowAnimName].length;
                 Animation.Sample();
+
+                if (Time.timeScale == 0)
+                {
+                    OnShown();
+                }
+            }
+            else
+            {
+                PlayAnimation(Animation, m_ShowAnimName);
             }
         }
 
         public override void Hide(bool hasAnimation)
         {
-            Animation.Play(m_HideAnimName);
-
             if (!hasAnimation)
             {
+                Animation.Play(m_HideAnimName);
                 Animation[m_HideAnimName].time = Animation[m_HideAnimName].length;
                 Animation.Sample();
+
+                if (Time.timeScale == 0)
+                {
+                    OnHidden();
+                }
             }
+            else
+            {
+                PlayAnimation(Animation, m_HideAnimName);
+            }
+        }
+
+        #if UNITY_EDITOR
+        protected override void Update()
+        {
+            base.Update();
+
+            if (Application.isPlaying)
+            {
+                AnimationUpdate();
+            }
+        }
+        #else
+        void Update()
+        {
+            AnimationUpdate();
+        }
+        #endif
+
+        void AnimationUpdate()
+        {
+            m_TimeAtCurrentFrame = UnityEngine.Time.realtimeSinceStartup;
+            m_DeltaTime = m_TimeAtCurrentFrame - m_TimeAtLastFrame;
+            m_TimeAtLastFrame = m_TimeAtCurrentFrame; 
+
+            if (m_IsPlaying)
+            {
+                if (m_IsEndAnim == true)
+                {
+                    m_CurrState.enabled = false;
+                    m_IsPlaying = false;
+
+                    if (Time.timeScale == 0)
+                    {
+                        if (m_CurrClipName == m_ShowAnimName)
+                        {
+                            OnShown();
+                        }
+                        else if (m_CurrClipName == m_HideAnimName)
+                        {
+                            OnHidden();
+                        }
+                    }
+
+                    return;
+                }
+
+                m_AccumTime += m_DeltaTime * Animation[m_CurrClipName].speed;
+                if (m_AccumTime >= m_CurrState.length)
+                {
+                    m_AccumTime = m_CurrState.length;
+                    m_IsEndAnim = true;
+                }
+                m_CurrState.normalizedTime = m_AccumTime / m_CurrState.length;
+            }
+        }
+
+        void PlayAnimation(Animation anim, string clip)
+        {
+            m_AccumTime = 0F;
+            m_CurrClipName = clip;
+            m_CurrState = anim[clip];
+            m_CurrState.weight = 1;
+            m_CurrState.blendMode = AnimationBlendMode.Blend;
+            m_CurrState.wrapMode = WrapMode.Once;
+            m_CurrState.normalizedTime = 0;
+            m_CurrState.enabled = true;
+            m_IsPlaying = true;
+            m_IsEndAnim = false;
         }
     }
 }
