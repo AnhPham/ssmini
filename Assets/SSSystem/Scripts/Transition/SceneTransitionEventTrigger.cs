4c82a341d2e165200086d2bdfd4bc4e7b4787962
Anh Pham <anhpt.csit@gmail.com>
2017-04-26 08:53:00 +0700

    Add License header to all scripts

--- Assets/SSSystem/Scripts/Transition/SceneTransitionEventTrigger.cs
+++ Assets/SSSystem/Scripts/Transition/SceneTransitionEventTrigger.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿// This code is part of the Mini SS-System library (https://github.com/AnhPham/ssmini) maintained by Anh Pham (anhpt.csit@gmail.com).
+// It is released for free under the MIT open source license (https://github.com/AnhPham/ssmini/blob/master/LICENSE)
+
+using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 
